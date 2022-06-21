8a5aedcdbffe07a2ace5ea86787eb9684fcfd131
OyindamolaB <oyinbanjoko@outlook.com>
2022-06-21 19:22:26 +0100

    My first commit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const operator = prompt('Enter operator to perform the calculation (either +, -, * or /):');
+const number1 = parseFloat(prompt ('Enter the first number:'));
+const number2 = parseFloat(prompt('Enter the second number:'));
+
+if (operator == '+')
+    result = number1 + number2;
+else if (operator == '-')
+     result = number1 - number2;
+else if (operator == '*')
+      result = number1 * number2;
+else if (operator == '/')
+    result = number1 / number2;
+
+    window.alert("Result is " + result);
